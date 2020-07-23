ccf3541a4b031c565dd8b51000451046bd10781a
Maksim <maksim-popov-1996@mail.ru>
2020-07-23 14:13:36 +0300

    Update 2020-07-23T11:13:29.131Z

--- script/script.js
+++ script/script.js
@@ -1,94 +1,44 @@
-(function() {
+let heightWindow,
+    menuIcon;
+heightWindow = document.documentElement.clientHeight;
+menuIcon = document.getElementById('menu-icon');
 
-    const properties = {
-        spaceDiameter : 30,
-        dotDiametr : 7,
-        wavelength : 250,
-        velocity : .017,
-        displacement : 1
-    }
+document.addEventListener('scroll', function() {
+    let scroll;
+    scroll = window.pageYOffset;
 
-    const canvas = document.getElementById('bg-home-page');
-    const ctx = canvas.getContext('2d');
-
-    let w = canvas.width = innerWidth;
-    let h = canvas.height = innerHeight;
-
-    let dotsList;
-
-    window.onresize = function() {
-        w = canvas.width = innerWidth;
-        h = canvas.height = innerHeight;
-        init();
-    }
-
-    class Dot {
-        constructor(x, y) {
-            this.x = x;
-            this.y = y;
-            this.radius = properties.dotDiametr / 2;
-            this.scale = getDistance(x, y) / properties.wavelength;
+    if (scroll < heightWindow) {
+        menuIcon.style.opacity = '0';
+        menuIcon.style.left = '-3em';
+        if (nav.classList.contains('menu-open')) {
+            nav.classList.remove('menu-open');
         }
-
-        update() {
-            this.resize();
-            this.draw();
-        }
-
-        resize() {
-            this.scale = this.scale - properties.velocity;
-        }
-
-        draw() {
-            let r = this.radius * (1 - Math.abs(Math.sin(this.scale)));
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, r, 0, 2 * Math.PI, false);
-            ctx.closePath();
-            ctx.fillStyle = 'rgba(255, 255, 255, .1)';
-            ctx.fill();
-        }
+    } else {
+        menuIcon.style.opacity = '1';
+        menuIcon.style.left = '2em';
     }
+}, false);
 
-    function init() {
-        dotsList = [];
 
-        const dotsCountX = w / properties.spaceDiameter | 0;
-        const dotsCountY = h / properties.spaceDiameter | 0;
-        const startX = (properties.spaceDiameter + w - dotsCountX * properties.spaceDiameter) / 2;
-        const startY = (properties.spaceDiameter + h - dotsCountY * properties.spaceDiameter) / 2;
+let nav;
+nav = document.getElementsByClassName('navigation')[0];
 
-        let displacement = properties.spaceDiameter / 4 * properties.displacement;
-        
-        for (let j = 0; j < dotsCountY; j++) {
-            displacement = -displacement
-            let y = startY + j * properties.spaceDiameter;
-            for (let i = 0; i < dotsCountX; i++) {
-                let x = startX + i * properties.spaceDiameter + displacement;
-                dotsList.push(new Dot(x, y));
-            }
-        } 
+menuIcon.addEventListener('click', function() {
+    if (nav.classList.contains('menu-open')) {
+        nav.classList.remove('menu-open');
+    } else {
+        nav.classList.add('menu-open');
+        closemenu();
     }
+}, false);
 
-    function loop() {
-        ctx.clearRect(0, 0, w, h)
-
-        for (let a in dotsList) {
-            dotsList[a].update();
-        }
-
-        requestAnimationFrame(loop);
+function closemenu() {
+    if (nav.classList.contains('menu-open')) {
+        document.querySelectorAll('.button').forEach(function(e) {
+            e.addEventListener('click', function() {
+                nav.classList.remove('menu-open');
+            }, false);
+        })
     }
+}
 
-    function getDistance(x, y) {
-        let dx = w / 2 - x;
-        let dy = h / 2 - y;
-        return Math.sqrt((dx * dx) + (dy * dy));
-    }
-    
-    dotsList = [new Dot()];
-    dotsList[0].draw();
-
-    init();
-    loop();
-
-})();
